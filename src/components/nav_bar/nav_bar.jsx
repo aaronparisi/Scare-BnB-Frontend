9d5c1dbaab80ae554bca81ffa670e8f55923c7db
Aaron <parisi.aaron@gmail.com>
2021-03-03 01:52:05 +0000

    messed w logo

--- src/components/nav_bar/nav_bar.jsx
+++ src/components/nav_bar/nav_bar.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { LoggedInBoolRoute } from '../../utils/route_util'
 import { SignInLinks, SignOutLinks } from './nav_bar_links'
-import logo from '../../images/fontImages/logo.png'
+import logoText from '../../images/fontImages/logo.png'
+import logo from '../../images/icons/family-logo.png'
 import HamburgerContainer from './hamburger_container'
 
 class NavBar extends React.Component {
@@ -25,8 +26,9 @@ class NavBar extends React.Component {
     return (
       <header className="nav-bar">
         <div className="nav-button-container">
-          <Link className="btn" to="/">
+          <Link className="btn logo-btn" to="/">
             <img className="nav-link-image logo" src={logo} alt="sbnb logo"/>
+            <img className="nav-link-image logo-text" src={logoText} alt="sbnb logo"/>
           </Link>
         </div>
         {/* <Link className="btn" to="/chirps">CHIRPS</Link> */}
