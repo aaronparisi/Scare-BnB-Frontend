d5cb2aa181a9517ab0a255212c38f4b0543b95d7
Aaron <parisi.aaron@gmail.com>
2021-03-03 17:55:41 +0000

    hamburger hover using styled component

--- src/components/nav_bar/hamburger.jsx
+++ src/components/nav_bar/hamburger.jsx
@@ -4,15 +4,28 @@ import { SignInLinks, SignOutLinks } from './nav_bar_links'
 import loginFace from '../../images/icons/login.png'
 import donut from '../../images/icons/donut.png'
 
+import styled, { ThemeProvider } from 'styled-components'
+
+const StyledDiv = styled.div`
+  border: ${props => props.theme.border};
+`
+
 const Hamburger = props => {
   const [expanded, setExpanded] = useState(false)
+  const [topHovered, setTopHovered] = useState(false)
+
+  const hoverTheme = (topHovered) ? {
+    border: '1px solid #FBD822'
+  } : {
+    border: '1px solid #D9A72D'
+  }
 
   const callSignInLinks = () => {
     return <SignInLinks />
   }
 
   const callSignOutLinks = () => {
-    return <SignOutLinks username={props.currentUser.username} logout={props.logout}/>
+    return <SignOutLinks username={props.currentUser.username} logout={props.logout} />
   }
 
   const MenuDisplay = () => {
@@ -25,13 +38,22 @@ const Hamburger = props => {
     }
   }
   return (
-    <div className="hamburger" onClick={() => setExpanded(!expanded)}>
-      <div className="hamburger-top">
-        <img src={donut} alt="donut" className="nav-link-image hamburger-dropdown"/>
-        <img className="nav-link-image hamburger-face" src={loginFace} alt="generic head"/>
-      </div>
-      <MenuDisplay />
-    </div>
+    <ThemeProvider theme={hoverTheme}>
+      <StyledDiv
+        className="hamburger" 
+        onClick={() => setExpanded(!expanded)}
+      >
+        <div
+          className="hamburger-top"
+          onMouseEnter={() => setTopHovered(true)}
+          onMouseLeave={() => setTopHovered(false)}
+        >
+          <img src={donut} alt="donut" className="nav-link-image hamburger-dropdown"/>
+          <img className="nav-link-image hamburger-face" src={loginFace} alt="generic head"/>
+        </div>
+        <MenuDisplay />
+      </StyledDiv>
+   </ThemeProvider>
   )
 }
 
--- src/components/nav_bar/nav_bar_links.jsx
+++ src/components/nav_bar/nav_bar_links.jsx
@@ -7,24 +7,37 @@ import logoutText from '../../images/fontImages/logout.png'
 
 export const SignInLinks = () => {
   return (
-    <div className="nav-button-container session-links">
-      <Link className="btn" to="/signup">
-        <img className="nav-link-image" src={signupText} alt="sign up"/>
-      </Link>
-      <Link className="btn" to="/login">
-        <img className="nav-link-image" src={loginText} alt="login"/>
-      </Link>
+    <div className="menu">
+      <div className="session-links">
+        <Link className="btn" to="/signup">
+          <img className="nav-link-image" src={signupText} alt="sign up"/>
+        </Link>
+        <Link className="btn" to="/login">
+          <img className="nav-link-image" src={loginText} alt="login"/>
+        </Link>
+      </div>
+      <div className="info-links">
+        {/* stuff about the site, contact, about us, etc. */}
+      </div>
     </div>
   )
 }
 
 export const SignOutLinks = ({ username, logout }) => {
   return (
-    <div className="nav-button-container session-links">
+    <div className="menu">
       {/* <p>Hello, {username}</p> */}
-      <button className="btn" onClick={logout}>
-        <img className="nav-link-image" src={logoutText} alt="logout"/>
-      </button>
+      <div className="session-links">
+        <button className="btn" onClick={logout}>
+          <img className="nav-link-image" src={logoutText} alt="logout"/>
+        </button>
+      </div>
+      <div className="info-links">
+        {/* maybe abstract this part */}
+      </div>
+      <div className="user-links">
+        {/* links to current user profile */}
+      </div>
     </div>
   )
 }
