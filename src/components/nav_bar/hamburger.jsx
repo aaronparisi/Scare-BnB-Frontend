13e089cea931a88c09688ae5ac064cd809b0cf64
aaronparisi <parisi.aaron@gmail.com>
2022-04-13 14:26:12 -0700

    modified dep array of Hamburger useEffect to account for null props.currentUser

--- src/components/nav_bar/hamburger.jsx
+++ src/components/nav_bar/hamburger.jsx
@@ -1,91 +1,92 @@
-import React, { useState, useEffect } from 'react'
-import { LoggedInBoolRoute } from '../../utils/route_util'
-import { SignInLinks, SignOutLinks } from './nav_bar_links'
-import loginFace from '../../images/icons/login.png'
-import donut from '../../images/icons/donut.png'
+import React, { useState, useEffect } from "react";
+import { LoggedInBoolRoute } from "../../utils/route_util";
+import { SignInLinks, SignOutLinks } from "./nav_bar_links";
+import loginFace from "../../images/icons/login.png";
+import donut from "../../images/icons/donut.png";
 
-import styled, { ThemeProvider } from 'styled-components'
-import { exchangeImageIdsForS3Urls } from '../../utils/properties_util'
+import styled, { ThemeProvider } from "styled-components";
+import { exchangeImageIdsForS3Urls } from "../../utils/properties_util";
 
 const StyledDiv = styled.div`
-  width: ${props => props.theme.width};
-  max-height: ${props => props.theme.maxHeight};
-  overflow: ${props => props.theme.overflow};
-  transition: width .5s, max-height .5s;
-`
+  width: ${(props) => props.theme.width};
+  max-height: ${(props) => props.theme.maxHeight};
+  overflow: ${(props) => props.theme.overflow};
+  transition: width 0.5s, max-height 0.5s;
+`;
 
 const StyledImg = styled.img`
-  padding: ${props => props.theme.padding};
-  filter: ${props => props.theme.filter};
-  transition: padding .5s;
-`
+  padding: ${(props) => props.theme.padding};
+  filter: ${(props) => props.theme.filter};
+  transition: padding 0.5s;
+`;
 
-const Hamburger = props => {
-  const [expanded, setExpanded] = useState(false)
-  const [redirectUrl, setRedirectUrl] = useState([])
+const Hamburger = (props) => {
+  console.log("inside hamburger component");
+  console.log(props);
+  const [expanded, setExpanded] = useState(false);
+  const [redirectUrl, setRedirectUrl] = useState([]);
 
   useEffect(() => {
-    if (props.currentUser !== null) {  // this feels hacky...
+    if (props.currentUser !== null) {
+      // this feels hacky...
       exchangeImageIdsForS3Urls([props.currentUser.avatar_url.id])
-      .then(data => {
-        setRedirectUrl(data[0].url)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+        .then((data) => {
+          setRedirectUrl(data[0].url);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }, [props.currentUser.avatar_url]) // watch out for array equality issues...?
+  }, [props.currentUser?.avatar_url]); // watch out for array equality issues...?
 
-  const burgerWidth = (expanded) ? '150px' : '110px'
-  const burgerMaxHeight = (expanded) ? '250px' : '48px'
+  const burgerWidth = expanded ? "150px" : "110px";
+  const burgerMaxHeight = expanded ? "250px" : "48px";
 
-  const loginAvatar = (
+  const loginAvatar =
     props.currentUser == null || props.currentUser.avatar_url == null
-  ) ? 
-  loginFace : 
-  redirectUrl
+      ? loginFace
+      : redirectUrl;
 
   const burgerTheme = {
     width: burgerWidth,
     maxHeight: burgerMaxHeight,
-    overflow: 'hidden'
-  }
+    overflow: "hidden",
+  };
 
   // padding: (expanded) ? '5px 7px' : (props.currentUser == null) ? '0px 0px' : '5px 7px',
   const faceTheme = {
-    padding: (expanded) ? '5px 7px' : '0px 0px',
-    filter: (props.currentUser == null || props.currentUser.avatar_url == null) ? 'invert(71%) sepia(58%) saturate(591%) hue-rotate(355deg) brightness(90%) contrast(87%)' : 'none'
-  }
+    padding: expanded ? "5px 7px" : "0px 0px",
+    filter:
+      props.currentUser == null || props.currentUser.avatar_url == null
+        ? "invert(71%) sepia(58%) saturate(591%) hue-rotate(355deg) brightness(90%) contrast(87%)"
+        : "none",
+  };
 
   const callSignInLinks = () => {
-    return <SignInLinks />
-  }
+    return <SignInLinks />;
+  };
 
   const callSignOutLinks = () => {
-    return <SignOutLinks userId={props.currentUser.id} logout={props.logout} />
-  }
+    return <SignOutLinks userId={props.currentUser.id} logout={props.logout} />;
+  };
 
   const MenuDisplay = () => {
     return (
-      <LoggedInBoolRoute 
-        trueComponent={callSignOutLinks} 
-        falseComponent={callSignInLinks} 
+      <LoggedInBoolRoute
+        trueComponent={callSignOutLinks}
+        falseComponent={callSignInLinks}
         path="/"
       />
-    )
-  }
+    );
+  };
 
   const genHamburgerFaceClass = () => {
     if (props.currentUser == null) {
-      return (
-        "nav-link-image hamburger-face"
-      )
+      return "nav-link-image hamburger-face";
     } else {
-      return ( 
-        "nav-link-image hamburger-avatar"
-      )
+      return "nav-link-image hamburger-avatar";
     }
-  }
+  };
 
   return (
     <ThemeProvider theme={burgerTheme}>
@@ -94,24 +95,27 @@ const Hamburger = props => {
         onMouseEnter={() => setExpanded(true)}
         onMouseLeave={() => setExpanded(false)}
       >
-        <div
-          className="hamburger-top"
-        >
-          <img src={donut} alt="donut" className="nav-link-image hamburger-dropdown"/>
+        <div className="hamburger-top">
+          <img
+            src={donut}
+            alt="donut"
+            className="nav-link-image hamburger-dropdown"
+          />
 
           <ThemeProvider theme={faceTheme}>
-            <StyledImg 
-              className={genHamburgerFaceClass()} 
+            <StyledImg
+              className={genHamburgerFaceClass()}
               src={loginAvatar}
               alt="generic head"
             />
           </ThemeProvider>
         </div>
-        
+
         <MenuDisplay />
       </StyledDiv>
-   </ThemeProvider>
-  )
-}
+    </ThemeProvider>
+  );
+};
 
-export default Hamburger
+export default Hamburger;
+
