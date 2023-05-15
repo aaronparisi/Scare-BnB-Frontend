3657e0f0fbba8aa252217d21f6fa1b3ef4d57c09
aaronparisi <parisi.aaron@gmail.com>
2023-05-14 22:12:07 -0700

    wiped dir clean

--- src/datePicker.js
+++
@@ -1,7 +0,0 @@
-$('#start_date').flatpickr({
-  "disable": [
-    date => {
-      return (date.getDay() === 0 || date.getDay() === 6)  // disable weekends
-    }
-  ]
-})
--- src/reducers/entities_reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-// import chirpsReducer from './chirps_reducer';
-
-export default combineReducers({
-  // chirps: chirpsReducer,
-});
